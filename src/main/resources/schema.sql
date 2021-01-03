ab70e7fcbfd416a5bd85a03e08a4b474874da303
mytun <lxyg06@163.com>
2021-01-03 16:17:53 +0800

    修改数据库

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,13 @@
+create table ROLE
+(
+    ID          VARCHAR(45) not null primary key,
+    NAME        VARCHAR(255)
+);
 create table USER
 (
     ID           VARCHAR(45) not null primary key,
     NAME         VARCHAR(255),
     OPENING_BANK VARCHAR(255),
-    PHONE_NUMBER VARCHAR(255)
+    PHONE_NUMBER VARCHAR(255),
+    ROLE_ID      VARCHAR(45)
 );
